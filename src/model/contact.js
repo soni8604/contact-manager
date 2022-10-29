d6086c2e4d6a050deebc82968cf56baf767ae0d1
santosh kumar soni <60810851+soni8604@users.noreply.github.com>
2022-10-29 14:53:10 +0530

    Update contact.js

--- src/model/contact.js
+++ src/model/contact.js
@@ -1,13 +1,13 @@
 const mongoose = require('mongoose')
 
 const contactsSchema = new mongoose.Schema({
-   name:{ type: String, required: true },
-    designation: { type: String, required: true },
-    company: { type: String, required: true },
-    industry: { type: String, required: true },
-    email: { type: String, required: true },
-    mobile: { type: Number, required: true },
-    country: { type: String, required: true },
+   name: String,
+    designation: String,
+    company: String,
+    industry: String,
+    email: String,
+    mobile: String,
+    country: String,
     userid: { type: mongoose.Schema.Types.ObjectId, ref: "User" }
 }, { timestamps: true })
 
